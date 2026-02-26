416ec8ff37a8141258e0578d84e908717974baac
BarisErdal <bariserdal.bme@gmail.com>
2026-02-26 23:02:58 +0300

    completed

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -2,11 +2,19 @@ package org.example.model;
 
 public class Store {
     public static void main(String[] args) {
-
-        //listProducts();
+ProductForSale[] products= new ProductForSale[5];
+products[0]= new Chocolate("sweet", 15,"chocolate desc");
+products[1]= new Bread("Bakery",5, "bread desc", "wholewheat", "brown");
+products[2]= new Coke("drinks", 20,"coke desc",true,2.5);
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
+for (ProductForSale product:products){
 
+    if(product != null) {
+        product.showDetails();
+    }
+}
     }
 }
